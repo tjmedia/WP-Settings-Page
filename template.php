38a6bc04e76812377d89ac53e2b87f7aca798ce7
tjmedia <thomas.jono@gmail.com>
2015-05-11 21:04:09 -0600

    Create template.php

---
+++ template.php
@@ -0,0 +1,10 @@
+<?php
+global $options;
+// this attaches the option name to the $variable for use in the template file
+$options = get_option('my_option_name', $options);
+?>
+
+<?php
+// this displays the option id specified from the WP Settings Page. In this case, option id is 'arctic_training_number'
+?>
+<?php echo $options['arctic_training_number']; ?>
